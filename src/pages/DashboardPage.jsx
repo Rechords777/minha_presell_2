af911868a2ecb5aef3832f40203527657d6f2352
Rechords777 <crs57008@gmail.com>
2025-06-06 16:56:52 -0300

    Fix: Remove extraneous quotes from DashboardPage

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,4 +1,4 @@
-'''import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { getPresells, deletePresell } from '../services/api';
 
@@ -172,4 +172,4 @@ function DashboardPage() {
 }
 
 export default DashboardPage;
-'''
+
